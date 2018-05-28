c93d2fe5b318e5a4fee56659b7598c7d41ec2ebc
Yanmagon <11396577+Yanmagon@users.noreply.github.com>
2018-05-28 16:49:37 +0200

    Fixed readline function

--- main.c
+++ main.c
@@ -18,11 +18,10 @@ void error(uint8_t *msg)
 	exit(EXIT_FAILURE);
 }
 
-int connect_server(void)
+int connect_server(int *socket_fd)
 {
 	struct addrinfo hints;
 	struct addrinfo *result, *result_p;
-	int socket_fd;
 
 	memset(&hints, 0, sizeof(struct addrinfo));
 	hints.ai_family = AF_INET;
@@ -32,17 +31,18 @@ int connect_server(void)
 	check = getaddrinfo("irc.freenode.net", "6667", &hints, &result);
 	if(check != 0)
 	{
-		error("getaddrinfo");
+		perror("getaddrinfo");
 		gai_strerror(check);
+		return -1;
 	}
 
 	for(result_p = result; result_p != NULL; result_p = result_p->ai_next)
 	{
-		socket_fd = socket(result_p->ai_family, result_p->ai_socktype,
+		*socket_fd = socket(result_p->ai_family, result_p->ai_socktype,
 				   result_p->ai_protocol);
-		if(socket_fd == 0)
+		if(*socket_fd == 0)
 		{
-			error("socket");
+			perror("socket");
 			break;
 		}
 
@@ -51,25 +51,26 @@ int connect_server(void)
 		char ip[INET6_ADDRSTRLEN];
 		int port = ntohs(addr->sin_port);
 
-		inet_ntop(result_p->ai_family, &addr->sin_addr, ip, INET6_ADDRSTRLEN);
+		inet_ntop(result_p->ai_family, &addr->sin_addr, ip,
+			  INET6_ADDRSTRLEN);
 		printf("ip: %s; port: %d; protocol: %d\n",
 		       ip, port, result_p->ai_protocol);
 
-		check = connect(socket_fd, result_p->ai_addr,
+		check = connect(*socket_fd, result_p->ai_addr,
 				result_p->ai_addrlen);
 		if(check == 0)
-		{
 			break;
 
-		}
-
-		error("connect");
-		close(socket_fd);		
+		close(*socket_fd);		
+	}
+	if(result_p == NULL)
+	{
+		perror("connect");
+		return -1;
 	}
 	freeaddrinfo(result);
 
-	printf("OK\n");
-	return socket_fd;
+	return 0;
 }
 
 void readline(int socket_fd, uint8_t *line)
@@ -77,75 +78,60 @@ void readline(int socket_fd, uint8_t *line)
 	char c;
 	int_fast16_t i;
 
-	printf("readline begin\n");
-	
 	for(i = 0; i <= MAX_MESSAGE_LENGTH; i++)
 	{
-		check = recv(socket_fd, c, 1, 0);
+		check = recv(socket_fd, &c, 1, 0);
 		if(check < 0)
 		{
-			printf("%d", i);
 			error("recv");
 		}
 
-		if(c == '\r')
+		if(c != '\r')
 		{
-			if((recv(socket_fd, c, 1, 0)) == '\n')
+			line[i] = c;
+		}else
+		{	
+			check = recv(socket_fd, &c, 1, MSG_PEEK);
+			if(check < 0)
+				error("recv peek");
+			
+			if(c == '\n')
 			{
-				*line = '\0';
-				printf("break\n");
+				line[i - 1] = '\0';
 				break;
 			}
-		}else
-			*line++ = c;
+		}
 	}
-
-	printf("readline end\n");
 }
 
 int main(int argc, char *argv[])
 {
 	int socket_fd;
 	uint8_t buffer[MAX_MESSAGE_LENGTH];
-	int_fast16_t i;
 	
-	socket_fd = connect_server();
-	for(i = 0; i < 2; i++)
-	{
-	check = recv(socket_fd, buffer, MAX_MESSAGE_LENGTH, 0);
+	check = connect_server(&socket_fd);
 	if(check < 0)
-	  error("read");
-	printf("%s\n", buffer);
+	{
+		printf("Error connecting to server");
+		exit(EXIT_FAILURE);
 	}
-	
+
+	//Send Identification messages
 	strncpy(buffer, "NICK tastytestbot\r\n", MAX_MESSAGE_LENGTH);
-	printf("%s #%d#\n", buffer, strlen(buffer));
 	check = send(socket_fd, buffer, strlen(buffer), 0);
 	if(check < 0)
 		error("send NICK");
-	
 	strncpy(buffer, "USER tastytestbot 0 * :tastytestbot\r\n",
 		MAX_MESSAGE_LENGTH);
-	printf("%s #%d#\n", buffer, strlen(buffer));
 	check = send(socket_fd, buffer, strlen(buffer), 0);
 	if(check < 0)
 		error("send USER");
-	
+
 	while(1)
 	{
-		check = recv(socket_fd, buffer, strlen(buffer), 0);
-		if(check < 0)
-			error("read");
-		printf("%s\n", buffer);
+		readline(socket_fd, line);
+		printf("%s", line);
 	}
-	int_fast8_t b = 0;
-	/* while(b < 2) */
-	/* { */
-	/* 	readline(socket_fd, line); */
-	/* 	printf("%s\n", line); */
-	/* 	b = 2; */
-	/* } */
 	
-	printf("OK");
 	return 0;
 }
